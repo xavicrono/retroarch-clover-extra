14135b0f6e5b527e391c1ffa63c00fe32bb62e9f
pcm720 <pcm720@gmail.com>
2017-03-19 22:25:21 +0300

    Updated

--- make_release.sh
+++ make_release.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 rm -rf ./release
 mkdir -p release/cores
-printf "\nPacking core template, BIOS template and CloverApp\n\n"
+mkdir -p release/extra_cores
+echo "\nPacking core template, BIOS template and CloverApp\n\n"
 zip -rq release/libretro_core_template.zip libretro_core_template.hmod/*
 zip -rq release/bios_template.zip bios_template.hmod/*
 zip -rq release/CloverApp.zip CloverApp
@@ -23,11 +24,15 @@ for dir in *.hmod; do
     tar -czf "../../release/extra_cores/$dir" *
     cd ../
 done
+cd ../
 mkdir -p release/additional_mods
 mv release/cores/use_* release/additional_mods
 mv release/cores/xmb_* release/additional_mods
+mv release/extra_cores/use_* release/additional_mods
 echo "\nPacking retroarch and main cores"
 cd release
-zip -rqv retroarch_with_cores.zip retroarch.hmod cores
+zip -rq retroarch_with_cores.zip retroarch.hmod cores
+echo "\nPacking extra cores"
+zip -rq extra_cores.zip extra_cores/*
 echo "\nPacking additional modules"
-zip -rqv additional_mods.zip additional_mods/*
+zip -rq additional_mods.zip additional_mods/*
